31f729aa1189c8a2f2618fa94e3fc672153bd63a
Bhupendra Kushwah <bhupendrakushwah977@gmail.com>
2023-12-11 20:39:45 +0530

    react chatBox by socket.io

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import io from 'socket.io-client'
+import Chat from './Component/Chat';
+
+const socket = io.connect("http://localhost:8080")
+
 
 function App() {
+
+  const [username, setUserName] = useState("")
+  const [room, setRoom] = useState("")
+  const [showChat, setShowChat] = useState(false)
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      const userData={
+        username: username,
+        room: room,
+      }
+      socket.emit("joinRoom", userData)
+      setShowChat(true)
+    }
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!showChat ?(<div className="joinChatContainer">
+      <h3>Join a Chat</h3>
+      <div className="joinForm">
+      <input type="text" placeholder="Enter the Name" onChange={(e) => { setUserName(e.target.value) }} />
+      <input type="text" placeholder="Room id" onChange={(e) => { setRoom(e.target.value) }} />
+      <button onClick={joinRoom}>Join A Room</button>
+      </div>
+      </div>):
+       (<Chat socket={socket} username={username} room={room}/>)}
     </div>
   );
 }
---
+++ src/Component/Chat.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
+
+const Chat = ({ socket, room, username }) => {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+  const [connectedUser, setConnectedUser] = useState("");
+
+  const sendMsg = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+      await socket.emit("send_message", messageData);
+      setCurrentMessage("")
+    }
+  };
+
+  useEffect(() => {
+    const handleReceiveMessage = (data) => {
+      setMessageList((list) => [...list, data]);
+    };
+
+    const handleUserConnected = (data) => {
+      setConnectedUser(data);
+      // Display a user join message
+      const joinMessage = {
+        author: "System",
+        message: `${data} joined the room`,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+        type: "join", // Added a type to identify system messages
+      };
+      setMessageList((list) => [...list, joinMessage]);
+    };
+
+    socket.on("receive_message", handleReceiveMessage);
+    socket.on("user-Connected", handleUserConnected);
+
+    return () => {
+      // Cleanup function to remove the event listeners when component unmounts
+      socket.off("receive_message", handleReceiveMessage);
+      socket.off("user-Connected", handleUserConnected);
+    };
+  }, [socket]);
+
+  return (
+    <div className="chat-window">
+      <div className="chat-header">
+        <p>ChatPulse</p>
+      </div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent, index) => (
+              
+            <div
+              key={index}
+              className={`message ${
+                messageContent.type === "join" ? "system-message" : ""
+              }`}
+              id={username === messageContent.author ? "you" : "other"}
+            >
+              <div>
+                <div className={`${ messageContent.type === "join"?'newUser':"message-content "}`}>
+                  <p>{messageContent.message}</p>
+                </div>
+                <div className="message-meta">
+                  <p id="time">{messageContent.type !== "join"&&messageContent.time}</p>
+                  <p id="author">{messageContent.type !== "join"&&messageContent.author}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </ScrollToBottom>
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          placeholder="write a message ..."
+          value={currentMessage}
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+        />
+        <button onClick={sendMsg}>&#9658;</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
